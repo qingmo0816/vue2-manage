1760e3516def266ffcbed0a5007755abbb537368
zuoan <pengyingnan0816@163.com>
2021-01-27 16:34:33 +0800

    创建login页面

--- src/page/login.vue
+++ src/page/login.vue
@@ -1,139 +1,101 @@
 <template>
-  	<div class="login_page fillcontain">
-	  	<transition name="form-fade" mode="in-out">
-	  		<section class="form_contianer" v-show="showLogin">
-		  		<div class="manage_tip">
-		  			<p>elm后台管理系统</p>
-		  		</div>
-		    	<el-form :model="loginForm" :rules="rules" ref="loginForm">
-					<el-form-item prop="username">
-						<el-input v-model="loginForm.username" placeholder="用户名"><span>dsfsf</span></el-input>
-					</el-form-item>
-					<el-form-item prop="password">
-						<el-input type="password" placeholder="密码" v-model="loginForm.password"></el-input>
-					</el-form-item>
-					<el-form-item>
-				    	<el-button type="primary" @click="submitForm('loginForm')" class="submit_btn">登陆</el-button>
-				  	</el-form-item>
-				</el-form>
-				<p class="tip">温馨提示：</p>
-				<p class="tip">未登录过的新用户，自动注册</p>
-				<p class="tip">注册过的用户可凭账号密码登录</p>
-	  		</section>
-	  	</transition>
-  	</div>
+  <div class="Login fillcontain">
+    <div class="login">
+      <div class="loginTitle">ele后台管理系统</div>
+      <div class="loginForm">
+        <el-form :model="rulesForm" ref="rulesForm" :rules="rules">
+          <el-form-item prop="name">
+            <el-input placeholder="用户名" v-model="rulesForm.name"></el-input>
+          </el-form-item>
+          <el-form-item prop="password">
+            <el-input
+              placeholder="密码"
+              v-model="rulesForm.password"
+              type="password"
+            ></el-input>
+          </el-form-item>
+          <el-button type="primary" @click="loginForm('rulesForm')"
+            >登录</el-button
+          >
+        </el-form>
+        <p class="text">温馨提示：</p>
+        <p class="text">未登录过的新用户，自动注册</p>
+        <p class="text">注册过的用户可凭账号密码登录</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-	import {login, getAdminInfo} from '@/api/getData'
-	import {mapActions, mapState} from 'vuex'
-
-	export default {
-	    data(){
-			return {
-				loginForm: {
-					username: '',
-					password: '',
-				},
-				rules: {
-					username: [
-			            { required: true, message: '请输入用户名', trigger: 'blur' },
-			        ],
-					password: [
-						{ required: true, message: '请输入密码', trigger: 'blur' }
-					],
-				},
-				showLogin: false,
-			}
-		},
-		mounted(){
-			this.showLogin = true;
-			if (!this.adminInfo.id) {
-    			this.getAdminData()
-    		}
-		},
-		computed: {
-			...mapState(['adminInfo']),
-		},
-		methods: {
-			...mapActions(['getAdminData']),
-			async submitForm(formName) {
-				this.$refs[formName].validate(async (valid) => {
-					if (valid) {
-						const res = await login({user_name: this.loginForm.username, password: this.loginForm.password})
-						if (res.status == 1) {
-							this.$message({
-		                        type: 'success',
-		                        message: '登录成功'
-		                    });
-							this.$router.push('manage')
-						}else{
-							this.$message({
-		                        type: 'error',
-		                        message: res.message
-		                    });
-						}
-					} else {
-						this.$notify.error({
-							title: '错误',
-							message: '请输入正确的用户名密码',
-							offset: 100
-						});
-						return false;
-					}
-				});
-			},
-		},
-		watch: {
-			adminInfo: function (newValue){
-				if (newValue.id) {
-					this.$message({
-                        type: 'success',
-                        message: '检测到您之前登录过，将自动登录'
-                    });
-					this.$router.push('manage')
-				}
-			}
-		}
-	}
+import { login } from "@/api/getData";
+export default {
+  data() {
+    return {
+      rulesForm: {
+        name: "",
+        password: "",
+      },
+      rules: {
+        name: [{ required: true, message: "请输入用户名", trigger: "blur" }],
+        password: [{ required: true, message: "请输入密码", trigger: "blur" }],
+      },
+    };
+  },
+  methods: {
+    async loginForm(rulesForm) {
+      this.$refs[rulesForm].validate(async (valid) => {
+        if (valid) {
+          const res = await login({
+            user_name: this.rulesForm.name,
+            password: this.rulesForm.password,
+          });
+          if (res.status === 1) {
+            this.$message({
+              type: "success",
+              message: res.success,
+            });
+            this.$router.push({ name: "home" });
+          }
+        }
+      });
+    },
+  },
+};
 </script>
 
 <style lang="less" scoped>
-	@import '../style/mixin';
-	.login_page{
-		background-color: #324057;
-	}
-	.manage_tip{
-		position: absolute;
-		width: 100%;
-		top: -100px;
-		left: 0;
-		p{
-			font-size: 34px;
-			color: #fff;
-		}
-	}
-	.form_contianer{
-		.wh(320px, 210px);
-		.ctp(320px, 210px);
-		padding: 25px;
-		border-radius: 5px;
-		text-align: center;
-		background-color: #fff;
-		.submit_btn{
-			width: 100%;
-			font-size: 16px;
-		}
-	}
-	.tip{
-		font-size: 12px;
-		color: red;
-	}
-	.form-fade-enter-active, .form-fade-leave-active {
-	  	transition: all 1s;
-	}
-	.form-fade-enter, .form-fade-leave-active {
-	  	transform: translate3d(0, -50px, 0);
-	  	opacity: 0;
-	}
+@import "../style/mixin.less";
+.Login {
+  background-color: rgb(50, 64, 87);
+}
+.login {
+  padding-top: 150px;
+  .loginTitle {
+    color: #fff;
+    text-align: center;
+    font-size: 22px;
+  }
+  .loginForm {
+    width: 350px;
+    height: 280px;
+    background-color: #fff;
+    margin: auto;
+    margin-top: 70px;
+    border-radius: 10px;
+    padding: 20px, 40px;
+    .el-form {
+      padding: 20px;
+    }
+    .el-button {
+      width: 310px;
+    }
+    .text {
+      font-size: 16px;
+      color: red;
+      text-align: center;
+    }
+  }
+}
+</style>
+<style>
 </style>
